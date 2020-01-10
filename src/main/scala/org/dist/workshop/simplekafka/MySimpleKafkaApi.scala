99d9b3c41269751218479e82f22763471d7126a9
Yashika-Tanwar <yashika_tanwar@external.mckinsey.com>
2020-01-10 16:25:34 +0530

    Add update meta data handler

--- src/main/scala/org/dist/workshop/simplekafka/MySimpleKafkaApi.scala
+++ src/main/scala/org/dist/workshop/simplekafka/MySimpleKafkaApi.scala
@@ -5,8 +5,9 @@ import org.dist.queue.api.{RequestKeys, RequestOrResponse}
 import org.dist.queue.common.TopicAndPartition
 import org.dist.queue.server.Config
 import org.dist.queue.utils.ZkUtils.Broker
-import org.dist.simplekafka.{LeaderAndReplicaRequest, PartitionInfo}
+import org.dist.simplekafka.{LeaderAndReplicaRequest, PartitionInfo, TopicMetadataRequest, TopicMetadataResponse, UpdateMetadataRequest}
 
+import scala.jdk.CollectionConverters._
 
 class MySimpleKafkaApi(config: Config, replicaManager: MyReplicaManager) {
   var aliveBrokers = List[Broker]()
@@ -26,8 +27,25 @@ class MySimpleKafkaApi(config: Config, replicaManager: MyReplicaManager) {
         })
         RequestOrResponse(RequestKeys.LeaderAndIsrKey, "", request.correlationId)
       }
+      case RequestKeys.UpdateMetadataKey ⇒ {
+        val updateMetadataRequest: UpdateMetadataRequest = JsonSerDes.deserialize(request.messageBodyJson.getBytes(), classOf[UpdateMetadataRequest])
+        aliveBrokers = updateMetadataRequest.aliveBrokers
+        updateMetadataRequest.leaderReplicas.foreach(leaderReplica ⇒ {
+          leaderCache.put(leaderReplica.topicPartition, leaderReplica.partitionStateInfo)
+        })
+        RequestOrResponse(RequestKeys.UpdateMetadataKey, "", request.correlationId)
+      }
+      case RequestKeys.GetMetadataKey ⇒ {
+        val topicMetadataRequest = JsonSerDes.deserialize(request.messageBodyJson.getBytes(), classOf[TopicMetadataRequest])
+        val topicAndPartitions = leaderCache.keySet().asScala.filter(topicAndPartition ⇒ topicAndPartition.topic == topicMetadataRequest.topicName)
+        val partitionInfo: Map[TopicAndPartition, PartitionInfo] = topicAndPartitions.map((tp: TopicAndPartition) ⇒ {
+          (tp, leaderCache.get(tp))
+        }).toMap
+        val topicMetadata = TopicMetadataResponse(partitionInfo)
+        RequestOrResponse(RequestKeys.LeaderAndIsrKey, JsonSerDes.serialize(topicMetadata), request.correlationId)
+      }
 
-      //      case _ ⇒ RequestOrResponse(0, "Unknown Request", request.correlationId)
+      case _ ⇒ RequestOrResponse(0, "Unknown Request", request.correlationId)
     }
   }
 }
--- src/test/scala/org/dist/worshop/simplekafka/MySimpleKafkaApiTest.scala
+++ src/test/scala/org/dist/worshop/simplekafka/MySimpleKafkaApiTest.scala
@@ -6,7 +6,7 @@ import org.dist.queue.common.TopicAndPartition
 import org.dist.queue.server.Config
 import org.dist.queue.utils.ZkUtils.Broker
 import org.dist.queue.{TestUtils, ZookeeperTestHarness}
-import org.dist.simplekafka.{LeaderAndReplicaRequest, LeaderAndReplicas, PartitionInfo, SimpleKafkaApi}
+import org.dist.simplekafka.{LeaderAndReplicaRequest, LeaderAndReplicas, PartitionInfo, SimpleKafkaApi, TopicMetadataResponse, UpdateMetadataRequest}
 import org.dist.util.Networks
 import org.dist.workshop.simplekafka.{MyReplicaManager, MySimpleKafkaApi}
 
@@ -24,6 +24,22 @@ class MySimpleKafkaApiTest extends ZookeeperTestHarness {
     assert(replicaManager.allPartitions.size() == 1)
     val partition = replicaManager.allPartitions.get(TopicAndPartition("topic1", 0))
     assert(partition.logFile.exists())
+  }
+
+  test("should update meta data req") {
+    val config = Config(0, new Networks().hostname(), TestUtils.choosePort(), zkConnect, List(TestUtils.tempDir().getAbsolutePath))
+    val replicaManager = new MyReplicaManager(config)
+    val simpleKafkaApi = new MySimpleKafkaApi(config, replicaManager)
+    val listOfLeadersAndBrokers = List(LeaderAndReplicas(TopicAndPartition("topic1", 0), PartitionInfo(Broker(1, "10.10.10.10", 8000), List(Broker(0, "10.10.10.10", 8000), Broker(1, "10.10.10.11", 8000)))))
+    val listOfBrokers = List(Broker(0, "10.10.10.10", 8000),Broker(1, "10.10.10.10", 8000))
+    val updateMetadataRequest = UpdateMetadataRequest(listOfBrokers,listOfLeadersAndBrokers)
+    val request = RequestOrResponse(RequestKeys.UpdateMetadataKey, JsonSerDes.serialize(updateMetadataRequest), 1)
+
+    simpleKafkaApi.handle(request)
+
+    assert(simpleKafkaApi.aliveBrokers == listOfBrokers)
+    assert(simpleKafkaApi.leaderCache.get(TopicAndPartition("topic1", 0)) == PartitionInfo(Broker(1, "10.10.10.10", 8000), List(Broker(0, "10.10.10.10", 8000), Broker(1, "10.10.10.11", 8000))))
+
   }
 
 }
